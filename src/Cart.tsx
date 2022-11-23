7478e515897043b57ec0c19feaa5937617162e68
Susanna Cho <susannacho@Susannas-MBP.gateway>
2022-11-23 22:34:29 +1100

    add cart logic, clear cart, calculate cart total

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -2,17 +2,20 @@ import React, { useState } from "react";
 
 const ads = [
   {
+    id: 1,
     name: "Classic Ad",
     description: "Offers the most basic level of advertisement",
     price: 269.99,
   },
   {
+    id: 2,
     name: "Stand out Ad",
     description:
       "Allows advertisers to use a company logo and use a longer presentation text",
     price: 322.99,
   },
   {
+    id: 3,
     name: "Premium Ad",
     description:
       "Same benefits as Standout Ad, but also puts the advertisement at the top of the results, allowing higher visibility",
@@ -20,8 +23,37 @@ const ads = [
   },
 ];
 
+const getAd = (id: number) => ads.find((ad) => ad.id === id);
+
+type CartItem = {
+  id: number;
+  qty: number;
+};
+
 const Cart = () => {
-  const [cart, setCart] = useState([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (id: number) => {
+    const cartItem = cartItems.find((item) => id === item.id) || { id, qty: 0 };
+    cartItem.qty++;
+    const otherCartItems = cartItems.filter((item) => id !== item.id);
+    // this wont preserve order, need to slice with index
+    setCartItems([...otherCartItems, cartItem]);
+  };
+
+  console.log(cartItems);
+
+  const calcCartTotal = () => {
+    const total = cartItems.reduce((sum, item) => {
+      const ad = getAd(item.id);
+      return ad ? ad.price * item.qty + sum : sum;
+    }, 0);
+    return total.toFixed(2);
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
 
   return (
     <div>
@@ -29,9 +61,18 @@ const Cart = () => {
         {ads.map((ad) => (
           <li>
             {ad.name} ${ad.price}
+            <button onClick={() => addToCart(ad.id)}>+</button>
           </li>
         ))}
       </ul>
+      {cartItems.map((item) => (
+        <>
+          <p>{getAd(item.id)?.name}</p>
+          <p>qty: {item.qty}</p>
+        </>
+      ))}
+      <p>Total ${calcCartTotal()}</p>
+      <button onClick={clearCart}>Clear cart</button>
     </div>
   );
 };
